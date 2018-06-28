46dd729e5a8696301daf76aad1018ab43b05ee3c
Danny.Jiang <ext.danny.jiang@ef.com>
2018-06-28 17:19:51 +0800

    delete all codes relative with AR

--- app/src/main/java/com/example/dannyjiang/myfirstar/MyFirstArActivity.java
+++ app/src/main/java/com/example/dannyjiang/myfirstar/MyFirstArActivity.java
@@ -4,24 +4,6 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.WindowManager;
-import android.widget.Toast;
-
-import com.example.dannyjiang.myfirstar.rendering.BackgroundRenderer;
-import com.example.dannyjiang.myfirstar.utils.CameraPermissionHelper;
-import com.example.dannyjiang.myfirstar.utils.TapHelper;
-import com.google.ar.core.ArCoreApk;
-import com.google.ar.core.Frame;
-import com.google.ar.core.Session;
-import com.google.ar.core.exceptions.CameraNotAvailableException;
-import com.google.ar.core.exceptions.UnavailableApkTooOldException;
-import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
-import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
-import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
-import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
-
-import java.io.IOException;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -32,14 +14,6 @@ public class MyFirstArActivity extends AppCompatActivity implements GLSurfaceVie
 
     // Surface View
     private GLSurfaceView surfaceView;
-    private TapHelper tapHelper;
-
-    // AR world
-    private Session session;
-    private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
-
-    // Permission stuff
-    private boolean installRequested;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +22,6 @@ public class MyFirstArActivity extends AppCompatActivity implements GLSurfaceVie
 
         // 初始化GLSurfaceView，并跟TapHelper绑定
         surfaceView = findViewById(R.id.glSurfaceView);
-        tapHelper = new TapHelper(/*context=*/ this);
-        surfaceView.setOnTouchListener(tapHelper);
 
         // 配置GLSurfaceView基本属性, 并设置renderer.
         surfaceView.setPreserveEGLContextOnPause(true);
@@ -63,131 +35,21 @@ public class MyFirstArActivity extends AppCompatActivity implements GLSurfaceVie
     protected void onResume() {
         super.onResume();
 
-        if (session == null) {
-            Exception exception = null;
-            String message = null;
-            try {
-                switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
-                    case INSTALL_REQUESTED:
-                        installRequested = true;
-                        return;
-                    case INSTALLED:
-                        break;
-                }
-
-                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
-                // permission on Android M and above, now is a good time to ask the user for it.
-                if (!CameraPermissionHelper.hasCameraPermission(this)) {
-                    CameraPermissionHelper.requestCameraPermission(this);
-                    return;
-                }
-
-                // Create the session.
-                session = new Session(/* context= */ this);
-
-            } catch (UnavailableArcoreNotInstalledException
-                    | UnavailableUserDeclinedInstallationException e) {
-                message = "Please install ARCore";
-                exception = e;
-            } catch (UnavailableApkTooOldException e) {
-                message = "Please update ARCore";
-                exception = e;
-            } catch (UnavailableSdkTooOldException e) {
-                message = "Please update this app";
-                exception = e;
-            } catch (UnavailableDeviceNotCompatibleException e) {
-                message = "This device does not support AR";
-                exception = e;
-            } catch (Exception e) {
-                message = "Failed to create AR session";
-                exception = e;
-            }
-
-            if (message != null) {
-                Log.e(TAG, "Exception creating session", exception);
-                return;
-            }
-        }
-
-        // Note that order matters - see the note in onPause(), the reverse applies here.
-        try {
-            session.resume();
-        } catch (CameraNotAvailableException e) {
-            // In some cases (such as another camera app launching) the camera may be given to
-            // a different app instead. Handle this properly by showing a message and recreate the
-            // session at the next iteration.
-            session = null;
-            return;
-        }
-
         surfaceView.onResume();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                    .show();
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this);
-            }
-            finish();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (session != null) {
-            // Note that the order matters - GLSurfaceView is paused first so that it does not try
-            // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
-            // still call session.update() and get a SessionPausedException.
-            surfaceView.onPause();
-            session.pause();
-        }
-    }
-
-    @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
-        try {
-            // 初始化用来画背景以及Virtual Object的OpenGL设置
-            // 主要包括各种OpenGL需要使用的Shader，Program等
-            backgroundRenderer.createOnGlThread(this);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to read an asset file", e);
-        }
+        GLES20.glClearColor(1f, 0f, 0f, 1.0f);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
-
-        // 当SurfaceView发生change时，需要重新设置Session的rotation, width, height
-        int displayRotation = getSystemService(WindowManager.class).getDefaultDisplay().getRotation();
-        session.setDisplayGeometry(displayRotation, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-        if (session == null) {
-            return;
-        }
-
-        try {
-            session.setCameraTextureName(backgroundRenderer.getTextureId());
-
-            // 通过AR Session获取当前手机摄像头(Camera)的当前帧(Frame)。
-            Frame frame = session.update();
-
-            // 将当前帧Frame当做背景来draw到SurfaceView上，因此我们能在手机屏幕上看到摄像头中的实时内容
-            backgroundRenderer.draw(frame);
-        } catch (Exception e) {
-
-        }
     }
 }
